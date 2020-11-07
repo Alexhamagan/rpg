20884c8d3b142dbe00e2187d3ffcae987231128e
Alexhamagan <45038699+Alexhamagan@users.noreply.github.com>
2020-11-07 06:17:53 +0200

    Add files via upload

---
+++ src/game.js
@@ -0,0 +1,46 @@
+import{Screen} from './screen';
+import{Loading} from './scenes/loading';
+import{Menu} from './scenes/menu';
+import{Scene} from './scene';
+
+export class Game{
+	constructor({width = 640, height = 640} = {}){
+		this.screen = new Screen(width, height);
+		this.screen.loadImages({
+			orc: 'img/orc.png',
+			player: 'img/player.png',
+			title: 'img/title.jpg',
+			tiled: 'img/tiles.png'
+		});
+		this.scenes = {
+			loading: new Loading(this),
+			menu: new Menu(this)
+		};
+		this.currentScene = this.scenes.loading;
+		this.currentScene.init();
+	}
+	
+	changeScene(status){
+		switch(status){
+			case Scene.LOADED:
+				return this.scenes.menu;
+				break;
+				
+			default:
+				return this.scenes.menu;
+		}
+	}
+	
+	frame(time){
+		if(this.currentScene.status != Scene.WORKING){
+			this.currentScene = this.changeScene(this.currentScene.status);
+			this.currentScene.init();
+		}
+		this.currentScene.render(time);
+		requestAnimationFrame(time => this.frame(time));
+	}
+	
+	run(){
+		requestAnimationFrame(time => this.frame(time));
+	}
+}
